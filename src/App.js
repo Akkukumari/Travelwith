a8efcaa27479409b6f9f24003d15dfeb716d1734
Akkukumari <akanksha28sbg@gmail.com>
2023-06-14 00:41:53 +0530

    fix: folder structure

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./app.scss";
+import Navbar from "./Components/Navbar/Navbar";
+import Home from "./Components/Home/Home";
+import Footer from "./Components/Footer/Footer";
+import Main from "./Components/Main/Main";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Main />
+      <Footer />
+    </>
+  );
+};
+export default App;
